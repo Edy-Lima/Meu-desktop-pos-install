ddf901ae86ee95a094f1faf5df81840fd90bc636
Edy-Lima <edivaldolima603@gmail.com>
2021-03-11 14:42:32 -0300

    Atualizado

--- pos-install-Xubuntu.sh
+++ pos-install-Xubuntu.sh
@@ -23,7 +23,7 @@ case $opcao in
                 sudo systemctl enable fstrim.timer
                 echo TRIM ativado
                 echo ***Agora vamos remover algumas coisas do sistema***
-                # Remove alguns softwares e o gnome-keyring.
+                ## Remove alguns softwares e o gnome-keyring.
                 sudo apt remove gnome-keyring -y
                 sudo apt remove --auto-remove gnome-keyring -y
                 sudo apt remove --purge snapd gnome-software-plugin-snap -y
@@ -32,7 +32,7 @@ case $opcao in
                 sudo apt remove --purge apport apport-gtk apport-symptoms -y
                 sudo apt autoremove --purge libreoffice* xfburn* atril* firefox* thunderbird* pidgin* -y          
                 echo Vamos agora remover o swap...
-                # Desativa e remove o swapfile!.
+                ## Desativa e remove o swapfile!.
                 sudo sed -i.bak '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab && sudo swapoff -a && sudo rm -f -r /swapfile
                 echo Swap removido!
                 echo ""
@@ -45,7 +45,7 @@ case $opcao in
                 sudo add-apt-repository ppa:cubic-wizard/release -y
                 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6494C6D6997C215E
                 sudo apt install cubic -y
-                # Instalar alguns programas extras.
+                ## Instalar alguns programas extras.
                 echo Instalando programas extras...
                 sudo apt install gnome-disk-utility exfat-fuse exfat-utils default-jre usb-creator-gtk -y 
                 sudo apt install xfce4-appmenu-plugin appmenu-gtk2-module appmenu-gtk3-module htop screenfetch -y 
@@ -59,10 +59,15 @@ case $opcao in
                 sudo add-apt-repository ppa:gezakovacs/ppa -y
                 sudo apt install unetbootin -y
                 echo Instalação do AnyDesk...
+                ## Adicionar chave de repositório à lista de fornecedores de software confiáveis:
                 sudo wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
+                ## Adicione o repositório:
                 sudo echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
-                sudo apt update && sudo apt install anydesk -y
-                # Desabilita a inicialização do Anydesk com o sistema!.
+                ## Atualizar o cache do apt:
+                sudo apt update
+                ## Instale o anydesk:
+                sudo apt install anydesk
+                ## Desabilita a inicialização do Anydesk com o sistema!.
                 sudo systemctl disable anydesk.service
                 echo Instalação do Git-GitHub...
                 sudo add-apt-repository ppa:git-core/ppa -y
@@ -72,7 +77,7 @@ case $opcao in
                 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
                 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
                 sudo apt install code -y
-                # Melhorar o desempenho do sistema.
+                ## Melhorar o desempenho do sistema.
                 echo Instalação do prelink e preload...
                 sudo rm /var/lib/dpkg/lock-frontend; sudo rm /var/cache/apt/archives/lock ;
                 sudo apt install preload prelink -y
@@ -95,12 +100,12 @@ case $opcao in
                 sudo systemctl enable fstrim.timer
                 echo TRIM ativado
                 echo ***Agora vamos remover algumas coisas do sistema***
-                # Remove alguns softwares e o gnome-keyring.
+                ## Remove alguns softwares e o gnome-keyring.
                 sudo apt remove gnome-keyring -y
                 sudo apt remove --auto-remove gnome-keyring -y
                 sudo apt remove --purge apport apport-gtk apport-symptoms -y
                 sudo apt autoremove --purge libreoffice* xfburn* atril* firefox* thunderbird* pidgin* -y 
-                # Instalar alguns programas extras
+                ## Instalar alguns programas extras
                 echo ***Aguarde! vamos instalar alguns programas***
                 echo Instalação do inkskape...
                 sudo add-apt-repository ppa:inkscape.dev/stable -y
@@ -122,10 +127,15 @@ case $opcao in
                 sudo add-apt-repository ppa:gezakovacs/ppa -y
                 sudo apt install unetbootin -y
                 echo Instalação do AnyDesk...
+                ## Adicionar chave de repositório à lista de fornecedores de software confiáveis:
                 sudo wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
+                ## Adicione o repositório:
                 sudo echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
-                sudo apt update && sudo apt install anydesk -y
-                # Desabilita a inicialização do Anydesk com o sistema!.
+                ## Atualizar o cache do apt:
+                sudo apt update
+                ## Instale o anydesk:
+                sudo apt install anydesk
+                ## Desabilita a inicialização do Anydesk com o sistema!.
                 sudo systemctl disable anydesk.service
                 echo Instalação do Git-GitHub...
                 sudo add-apt-repository ppa:git-core/ppa -y
@@ -135,7 +145,7 @@ case $opcao in
                 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
                 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
                 sudo apt install code -y
-                # Melhorar o desempenho do sistema.
+                ## Melhorar o desempenho do sistema.
                 echo Instalação do prelink e preload...
                 sudo rm /var/lib/dpkg/lock-frontend; sudo rm /var/cache/apt/archives/lock ;
                 sudo apt install preload prelink -y
